cde3f42b877295bf78175046f4691f29594976e4
Muhammad-Jay <jsync4172004@gmail.com>
2025-12-16 15:26:10 +0100

    feat(desktop): Refactor main window UI and introduce side panels
    This commit refactors the main window UI of the desktop application, introducing a new layout with side panels and dialog functionality.
    
    Key changes include:
    - ****: Implemented a  and  layout for the main window, replacing the previous content control. Introduced a sliding side panel controlled by  and added navigation buttons.
    - ****: Added  observable property and ,  relay commands to manage the new UI elements.
    - ****: Removed old  related to .
    - ****: Updated with a temporary placeholder text.
    
    These changes enhance the desktop application's user interface and prepare it for more interactive features.

--- Jaguar.Desktop/ViewModels/MainWindowViewModel.cs
+++ Jaguar.Desktop/ViewModels/MainWindowViewModel.cs
@@ -1,13 +1,31 @@
 using CommunityToolkit.Mvvm.ComponentModel;
-using ReactiveUI;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Jaguar.Desktop.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
-        [ObservableProperty]
-        private ViewModelBase _content;
+        [ObservableProperty] private ViewModelBase _content;
+        [ObservableProperty] private string _isDialogOpen = "Left";
+
+        [RelayCommand]
+        private void OpenDialog()
+        {
+            if (IsDialogOpen == "Left")
+            {
+                IsDialogOpen = "Right";
+            }
+            else
+            {
+                IsDialogOpen = "Left";
+            }
+        }
         
+        [RelayCommand]
+        private void SetContent()
+        {
+            Content = new WorkflowViewModel();
+        }
         public MainWindowViewModel()
         {
             Content = new WorkflowViewModel();
